6bb189e5f4600efa62d95ab93673ba92e152af02
NicholasJW <yunwen.wan@richmond.edu>
2017-09-27 00:25:31 -0400

    I'm pissed by intelliJ.

--- src/com/urcssa/Gui.java
+++ src/com/urcssa/Gui.java
@@ -1,28 +1,64 @@
 
 /**
  * This class constructs a window for a event.
- * 
- * @author (Nicholas Wan) 
+ *
+ * @author (Nicholas Wan)
  * @version (09/26/2017)
  */
 
+//
+//import java.awt.*;
+//import java.io.File;
+//import squint.*;
+//import javax.swing.*;
+//import java.util.*;
+////import java.lang.Object;
+//
+//
+//public class Gui extends GUIManager{
+//
+////	private ImageViewer cover;
+////	private ImageViewer secret;
+////	private ImageViewer encrypted;
+////	private JSplitPane split;
+//	private JPanel bigPanel;
+//	private JTextArea theTextArea;
+////	private JButton encodeButton;
+////	private JButton decodeButton;
+////	private JButton encryptButton;
+////	private JButton decryptButton;
+////	private JButton clearTextButton;
+////	private JButton clearImageButton;
+////	private JPanel buttonPanel;
+////	private JButton whichButton;
+//
+//	public Gui(){
+//		// Default constructor
+//		bigPanel = new JPanel();
+//	}
+//
+//	public void buttonClicked(){
+//		// function to call if the button is clicked
+//	}
+//
+//	public void dropdownClicked(){
+//		// function to call if the dropdown menu is clicked
+//	}
+//
+//	public static void main(){
+//		system.out.println("hello world");
+//	}
+//}
+
 import java.awt.*;
-import java.io.File;
-import squint.*;
+//import java.awt.event;
 import javax.swing.*;
-import java.util.*;
 
-
-public class Gui extends GUIManager{
+public class Gui{
 	public Gui(){
-		// Default constructor
-	}
 
-	public void buttonClicked(){
-		// function to call if the button is clicked
 	}
-
-	public void dropdownClicked(){
-		// function to call if the dropdown menu is clicked
+	public static void main(String[] args){
+		new Gui();
 	}
 }
